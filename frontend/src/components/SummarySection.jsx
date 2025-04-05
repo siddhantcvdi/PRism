2a4e5317b9413ca526c1670c5a8b456a9d48ebb2
Sambodhi-Roy <sambodhiroy2005@gmail.com>
2025-04-06 02:37:05 +0530

    Frontend Dashboard Enhancement

--- frontend/src/components/SummarySection.jsx
+++ frontend/src/components/SummarySection.jsx
@@ -1,13 +1,17 @@
 import { UserButton } from "@clerk/clerk-react";
 import React from "react";
 import PRCard from "./PRCard";
+import { GrSync } from "react-icons/gr";
 
 const SummarySection = () => {
   return (
     <div className="w-full h-screen bg-[#1d1e30] p-3">
       <div className="w-full h-full bg-[#131420] rounded-xl">
         <div className="flex w-full py-4 px-6 justify-end">
-          <UserButton />
+          <div className="flex gap-4 justify-center items-center">
+            <GrSync className="text-white cursor-pointer" />
+            <UserButton />
+          </div>
         </div>
       </div>
     </div>
